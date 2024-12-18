3ebadf0672f6ac92e3e203ac783d256034441bb0
owolfdev <owolfdev@gmail.com>
2024-12-19 05:17:41 +0700

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,7 +4,7 @@ const path = require("node:path");
 
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://www.thrivingexpat.com/",
+  siteUrl: process.env.SITE_URL || "https://poodeek.vercel.app/",
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   exclude: [
