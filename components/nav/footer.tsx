6c0c82cdd3a1c2cebc084968d7029b23090a7055
owolfdev <owolfdev@gmail.com>
2025-01-01 08:15:51 +0700

    update

--- components/nav/footer.tsx
+++ components/nav/footer.tsx
@@ -9,18 +9,22 @@ function Footer() {
           <div className="">&copy; {new Date().getFullYear()} website.com</div>
           {/* <ThemeSwitcher /> */}
         </div>
-        <nav className="flex gap-4 items-center">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-          {/* <Link href="/blog">Blog</Link> */}
-          <Link href="/privacy">Privacy</Link>
-          <Link
-            href="/donate"
-            className="bg-yellow-300 px-3 py-1 rounded-full font-bold"
-          >
-            Donate
-          </Link>
+        <nav className="flex sm:flex-row flex-col w-full justify-center items-center gap-4">
+          <div className="flex gap-4 items-center">
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            {/* <Link href="/blog">Blog</Link> */}
+            <Link href="/privacy">Privacy</Link>
+          </div>
+          <div className="sm:pb-0 pb-8">
+            <Link
+              href="/donate"
+              className="bg-yellow-300 px-3 py-1 rounded-full font-bold sm:text-xl text-2xl"
+            >
+              Donate
+            </Link>
+          </div>
         </nav>
       </div>
     </footer>
